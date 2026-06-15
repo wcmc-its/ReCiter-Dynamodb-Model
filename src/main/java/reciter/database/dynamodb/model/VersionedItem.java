0560d6e3e2eb611045837fe7682754d204fd699f
Veerraju <Veerraju@192.168.13.237>
2026-06-15 13:57:05 +0530

    schemaVersion added in analysisout model

--- src/main/java/reciter/database/dynamodb/model/VersionedItem.java
+++ src/main/java/reciter/database/dynamodb/model/VersionedItem.java
@@ -1,7 +1,24 @@
 package reciter.database.dynamodb.model;
 
+/**
+ * Represents a persistable item whose structure is versioned.
+ * Implementations use the schema version to support data migrations
+ * and backward compatibility across schema revisions.
+ */
 public interface VersionedItem {
 
+	/**
+     * Gets the schema version associated with this item.
+     *
+     * @return the schema version used to interpret the item's data.
+     */
+	
 	int getSchemaVersion();
+	
+	/**
+     * Sets the schema version associated with this item.
+     *
+     * @param version the schema version to assign to this item.
+     */
     void setSchemaVersion(int version);
 }
